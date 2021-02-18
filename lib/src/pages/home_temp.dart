78578b95e3f81403c07e8859dd54906ca4de3741
Elis Alvarez <elisalvarez@msn.com>
2021-02-19 00:12:35 +0100

    Inicio Proyecto

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('ListTile'),
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
